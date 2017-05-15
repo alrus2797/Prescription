1885521e0496a2fbb0b5d0835ba57f67f50f02de
luisbch9 <luisbch9@gmail.com>
2017-05-15 11:42:16 -0500

    librerias JS

---
+++ resources/views/Medicamentos/create.blade.php
@@ -0,0 +1,9 @@
+<script src="{{asset('react.js')}}"></script>
+<script src="{{asset('react-dom.js')}}"></script>
+<script src="{{asset('babel.min.js')}}"></script>
+<script src="{{asset('axios.min.js')}}"></script>
+
+
+<div id="app"></div>
+
+
