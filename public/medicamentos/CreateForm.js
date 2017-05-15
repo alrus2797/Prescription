1eabb3b2f5b609bd33e3282c7dd43112c2966839
luisbch9 <luisbch9@gmail.com>
2017-05-15 12:30:06 -0500

    Create Ejemplo react

---
+++ public/medicamentos/CreateForm.js
@@ -0,0 +1,26 @@
+class CreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+ 
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
--- resources/views/Medicamentos/create.blade.php
+++ resources/views/Medicamentos/create.blade.php
@@ -1,9 +1,22 @@
-<script src="{{asset('react.js')}}"></script>
-<script src="{{asset('react-dom.js')}}"></script>
-<script src="{{asset('babel.min.js')}}"></script>
-<script src="{{asset('axios.min.js')}}"></script>
+<script src="{{asset('js/react.js')}}"></script>
+<script src="{{asset('js/react-dom.js')}}"></script>
+<script src="{{asset('js/babel.min.js')}}"></script>
+<script src="{{asset('js/axios.min.js')}}"></script>
+
+Formulario
+<div id="root"></div>
+
+<script type="text/babel" src="{{asset('medicamentos/CreateForm.js')}}">
+</script>
 
 
-<div id="app"></div>
+<script type="text/babel">
 
+	
+	const formulario = <CreateForm />;
 
+	ReactDOM.render(
+		formulario,
+		document.getElementById('root')
+  	);
+</script>
